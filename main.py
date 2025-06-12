cafe9dd210d799ce2f2a120f5d1c82f3e9cef2c4
Camilo Benitez <camilo.benitez@domotes.co>
2025-06-12 14:54:36 -0500

    second commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "¡Hola desde Flask en Vercel!"})
+
+
+# Este bloque es necesario para que Vercel detecte el handler
+def handler(environ, start_response):
+    return app(environ, start_response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
